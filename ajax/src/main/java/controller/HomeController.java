be7023b96aae5e1d686be980da1b4d9374a7d106
akshatayevatkar <a_yevatkar@outlook.com>
2018-10-26 13:37:45 +0530

    add...!

--- ajax/src/main/java/controller/HomeController.java
+++ ajax/src/main/java/controller/HomeController.java
@@ -1,6 +1,10 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class HomeController {
+	
 public static void main(String[] args) {
 	System.out.println("In controller");
 }
